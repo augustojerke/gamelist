64611d656a3b8476d9064cbe8974ce990de0455c
augusto.jerke <augustowieles@gmail.com>
2025-02-24 19:48:29 -0300

    title and color

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { ModeToggle } from "@/components/common/dark-mode-toggle";
+import GamelistTitle from "@/components/common/gamelist-title";
 import { SignUpForm } from "@/components/forms/signup-form";
 
 export default function Page() {
   return (
     <div className="relative min-h-svh w-full flex items-center justify-center p-6 md:p-10">
+      <div className="absolute top-4 left-4">
+        <GamelistTitle className="text-3xl" />
+      </div>
       <div className="absolute top-4 right-4">
         <ModeToggle />
       </div>
