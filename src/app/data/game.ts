36f6e6bd12fbfa8ce3667f180d2aa17db8e36389
augusto.jerke <augusto.jerke@abase.com.br>
2025-03-27 15:39:18 -0300

    merge

--- src/app/data/game.ts
+++ src/app/data/game.ts
@@ -2,6 +2,7 @@ import { AxiosPromise } from "axios";
 import apiIgdb from "../api/api-igdb";
 import { Game } from "@/types/game";
 import { GameTableFiltersInterface } from "@/types/game-table-filters";
+import { getGenresId } from "./genres";
 
 export async function getGames(
   limit: number,
@@ -16,16 +17,14 @@ export async function getGames(
     bodyParams += ` & name ~ *"${searchGameName}"*`;
   }
 
-  if(filters.genres.length != 0){
-    bodyParams += ` & genres.checksum = (${filters.genres.join(",")})`
-  }
-  console.log(filters)
-  console.log(bodyParams)
+  console.log(await getGenresId(filters.genres));
 
   bodyParams += `
     ;limit ${limit}; 
     offset ${offset};
     sort total_rating ${filters.order};`;
+
+  console.log(bodyParams)
 
   const response = await apiIgdb.post("/getGames", bodyParams);
   return response;
